dacd7241625913d1099c42d88be067ba9db4b87a
liuenxiang.ex <liuenxiang.ex@hx-partners.com>
2025-10-22 15:45:04 +0800

    stringtest

---
+++ BenchmarkTest/LoopTest.cs
@@ -0,0 +1,381 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.Text;
+
+[MemoryDiagnoser]//记录内存分配情况
+public class LoopBenchmark
+{
+    public IEnumerable<object> Source => new object[] { 1 };
+
+    public IEnumerable<object[]> Source1 => new object[][] {
+        new object[] { 1, 0 }
+    };
+
+    [Benchmark]
+    [ArgumentsSource(nameof(Source))]
+    public int ForLoop(int n)
+    {
+        int res = 0;
+        // 循环求和 1, 2, ..., n-1, n
+        for (int i = 1; i <= n; i++)
+        {
+            res += i;
+        }
+        return res;
+    }
+
+    [Benchmark]
+    [ArgumentsSource(nameof(Source))]
+    public int WhileLoop(int n)
+    {
+        int res = 0;
+        int i = 1; // 初始化条件变量
+                   // 循环求和 1, 2, ..., n-1, n
+        while (i <= n)
+        {
+            res += i;
+            i += 1; // 更新条件变量
+        }
+        return res;
+    }
+
+    [Benchmark]
+    [ArgumentsSource(nameof(Source))]
+    public int WhileLoopII(int n)
+    {
+        int res = 0;
+        int i = 1; // 初始化条件变量
+                   // 循环求和 1, 4, 10, ...
+        while (i <= n)
+        {
+            res += i;
+            // 更新条件变量
+            i += 1;
+            i *= 2;
+        }
+        return res;
+    }
+
+    [Benchmark]
+    [ArgumentsSource(nameof(Source))]
+    public string NestedForLoop(int n)
+    {
+        StringBuilder res = new();
+        // 循环 i = 1, 2, ..., n-1, n
+        for (int i = 1; i <= n; i++)
+        {
+            // 循环 j = 1, 2, ..., n-1, n
+            for (int j = 1; j <= n; j++)
+            {
+                res.Append($"({i}, {j}), ");
+            }
+        }
+        return res.ToString();
+    }
+
+    [Benchmark]
+    [ArgumentsSource(nameof(Source))]
+    public int Recur(int n)
+    {
+        // 终止条件
+        if (n == 1)
+            return 1;
+        // 递：递归调用
+        int res = Recur(n - 1);
+        // 归：返回结果
+        return n + res;
+    }
+
+    [Benchmark]
+    [ArgumentsSource(nameof(Source1))]
+    public int TailRecur(int n, int res = 0)
+    {
+        // 终止条件
+        if (n == 0)
+            return res;
+        // 尾递归调用
+        return TailRecur(n - 1, res + n);
+    }
+
+    [Benchmark]
+    [ArgumentsSource(nameof(Source))]
+    public int Fib(int n)
+    {
+        // 终止条件 f(1) = 0, f(2) = 1
+        if (n == 1 || n == 2)
+            return n - 1;
+        // 递归调用 f(n) = f(n-1) + f(n-2)
+        int res = Fib(n - 1) + Fib(n - 2);
+        Console.WriteLine($"Fib({n}) = {res}");
+        // 返回结果 f(n)
+        return res;
+    }
+
+    [Benchmark]
+    public int BubbleSort(int[] nums)
+    {
+        int count = 0;  // 计数器
+                        // 外循环：未排序区间为 [0, i]
+        for (int i = nums.Length - 1; i > 0; i--)
+        {
+            // 内循环：将未排序区间 [0, i] 中的最大元素交换至该区间的最右端
+            for (int j = 0; j < i; j++)
+            {
+                if (nums[j] > nums[j + 1])
+                {
+                    // 交换 nums[j] 与 nums[j + 1]
+                    (nums[j + 1], nums[j]) = (nums[j], nums[j + 1]);
+                    count += 3;  // 元素交换包含 3 个单元操作
+                }
+            }
+        }
+        return count;
+    }
+
+    [Benchmark]
+    public int Exponential(int n)
+    {
+        int count = 0, bas = 1;
+        // 细胞每轮一分为二，形成数列 1, 2, 4, 8, ..., 2^(n-1)
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < bas; j++)
+            {
+                count++;
+            }
+            bas *= 2;
+        }
+        // count = 1 + 2 + 4 + 8 + .. + 2^(n-1) = 2^n - 1
+        return count;
+    }
+
+    [Benchmark]
+    public int ExpRecur(int n)
+    {
+        if (n == 1) return 1;
+        return ExpRecur(n - 1) + ExpRecur(n - 1) + 1;
+    }
+
+    [Benchmark]
+    public int Logarithmic(int n)
+    {
+        int count = 0;
+        while (n > 1)
+        {
+            n /= 2;
+            count++;
+        }
+        return count;
+    }
+
+    [Benchmark]
+    public int LogRecur(int n)
+    {
+        if (n <= 1) return 0;
+        return LogRecur(n / 2) + 1;
+    }
+
+    [Benchmark]
+    public int LinearLogRecur(int n)
+    {
+        if (n <= 1) return 1;
+        int count = LinearLogRecur(n / 2) + LinearLogRecur(n / 2);
+        for (int i = 0; i < n; i++)
+        {
+            count++;
+        }
+        return count;
+    }
+
+    [Benchmark]
+    public int[] RandomNumbers(int n)
+    {
+        int[] nums = new int[n];
+        // 生成数组 nums = { 1, 2, 3, ..., n }
+        for (int i = 0; i < n; i++)
+        {
+            nums[i] = i + 1;
+        }
+
+        // 随机打乱数组元素
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int index = new Random().Next(i, nums.Length);
+            (nums[i], nums[index]) = (nums[index], nums[i]);
+        }
+        return nums;
+    }
+
+    [Benchmark]
+    public int FindOne(int[] nums)
+    {
+        for (int i = 0; i < nums.Length; i++)
+        {
+            // 当元素 1 在数组头部时，达到最佳时间复杂度 O(1)
+            // 当元素 1 在数组尾部时，达到最差时间复杂度 O(n)
+            if (nums[i] == 1)
+                return i;
+        }
+        return -1;
+    }
+
+    /* 类 */
+
+    internal class Node
+    {
+        public Node(int x)
+        {
+            int val = x;
+            Node next;
+        }
+    }
+
+    /* 函数 */
+
+    private int Function()
+    {
+        // 执行某些操作...
+        return 0;
+    }
+
+    public int Algorithm(int n)
+    {        // 输入数据
+        const int a = 0;          // 暂存数据（常量）
+        int b = 0;                // 暂存数据（变量）
+        Node node = new(0);       // 暂存数据（对象）
+        int c = Function();       // 栈帧空间（调用函数）
+        return a + b + c;         // 输出数据
+    }
+
+    public void Algorithm2(int n)
+    {
+        int a = 0;                   // O(1)
+        int[] b = new int[10000];    // O(1)
+        if (n > 10)
+        {
+            int[] nums = new int[n]; // O(n)
+        }
+    }
+
+    /* 循环的空间复杂度为 O(1) */
+    void Loop(int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            Function();
+        }
+    }
+    /* 递归的空间复杂度为 O(n) */
+    int Recur2(int n)
+    {
+        if (n == 1) return 1;
+        return Recur2(n - 1);
+    }
+
+    /* 常数阶 */
+    void Constant(int n)
+    {
+        // 常量、变量、对象占用 O(1) 空间
+        int a = 0;
+        int b = 0;
+        int[] nums = new int[10000];
+        //ListNode node = new(0);
+        // 循环中的变量占用 O(1) 空间
+        for (int i = 0; i < n; i++)
+        {
+            int c = 0;
+        }
+        // 循环中的函数占用 O(1) 空间
+        for (int i = 0; i < n; i++)
+        {
+            Function();
+        }
+    }
+
+    /* 线性阶 */
+    void Linear(int n)
+    {
+        // 长度为 n 的数组占用 O(n) 空间
+        int[] nums = new int[n];
+        // 长度为 n 的列表占用 O(n) 空间
+        List<ListNode> nodes = new List<ListNode>();
+        for (int i = 0; i < n; i++)
+        {
+            nodes.Add(new ListNode(i));
+        }
+        // 长度为 n 的哈希表占用 O(n) 空间
+        Dictionary<int, string> map = new Dictionary<int, string>();
+        for (int i = 0; i < n; i++)
+        {
+            map.Add(i, i.ToString());
+        }
+    }
+
+    /* 线性阶（递归实现） */
+    void LinearRecur(int n)
+    {
+        Console.WriteLine("递归 n = " + n);
+        if (n == 1) return;
+        LinearRecur(n - 1);
+    }
+
+    /* 平方阶 */
+    void Quadratic(int n)
+    {
+        // 矩阵占用 O(n^2) 空间
+        int[,] numMatrix = new int[n, n];
+        // 二维列表占用 O(n^2) 空间
+        List<List<int>> numList = new List<List<int>>();
+        for (int i = 0; i < n; i++)
+        {
+            List<int> tmp = new List<int>();
+            for (int j = 0; j < n; j++)
+            {
+                tmp.Add(0);
+            }
+            numList.Add(tmp);
+        }
+    }
+
+    /* 平方阶（递归实现） */
+    int QuadraticRecur(int n)
+    {
+        if (n <= 0) return 0;
+        int[] nums = new int[n];
+        Console.WriteLine("递归 n = " + n + " 中的 nums 长度 = " + nums.Length);
+        return QuadraticRecur(n - 1);
+    }
+
+    /* 指数阶（建立满二叉树） */
+    TreeNode? BuildTree(int n)
+    {
+        if (n == 0) return null;
+        TreeNode root = new TreeNode(0)
+        {
+            left = BuildTree(n - 1),
+            right = BuildTree(n - 1)
+        };
+        return root;
+    }
+}
+
+internal class ListNode
+{
+    private int i;
+
+    public ListNode(int i)
+    {
+        this.i = i;
+    }
+}
+
+internal class TreeNode
+{
+    internal TreeNode left;
+    internal TreeNode right;
+    private int v;
+
+    public TreeNode(int v)
+    {
+        this.v = v;
+    }
+}
---
+++ TestProject2/UnitTest2.cs
@@ -0,0 +1,147 @@
+﻿using FurionTest.Application.Interfaces;
+using FurionTest.Core.Models;
+using Xunit.Abstractions;
+
+namespace TestProject2;
+
+public class UnitTest2
+{
+    private readonly ITestOutputHelper Output;
+
+    public UnitTest2(
+    ITestOutputHelper tempOutput)
+    {
+        Output = tempOutput;
+    }
+
+    [Fact]
+    public void 测试生成数组()
+    {
+        int[] arr = [1, 2, 3, 4, 5];
+        List<string> list = ["a", "b", "c"];
+        Span<char> span = ['a', 'b', 'c'];
+        int[][] two2D = [[1, 2], [3, 4], [5, 6]];
+
+        int[] item0 = [88, 2, 3];
+        int[] item1 = [88, 2, 3, 4, 5];
+        int[] item2 = [88, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        int[] single = [.. item0, .. item1, .. item2];
+        foreach (var item in single)
+        {
+            Output.WriteLine(item.ToString());
+        }
+    }
+
+    [Fact]
+    public void Test_StudentInfo()
+    {
+        var femaleStudents = students.Where(s => s.StudentName == "时光者");
+        var studentNames = students.Select(s => s.StudentName);
+
+        // 使用SelectMany展平所有学生的课程列表
+        var allCourses = students.SelectMany(student => student.Courses).ToList();
+
+        // 输出所有课程的名称
+        foreach (var course in allCourses)
+        {
+            Output.WriteLine(course.CourseName);
+        }
+
+        var studentList = students.ToList();
+        var studentArray = students.ToArray();
+        var studentDictionary = students.ToDictionary(s => s.StudentID, s => s.StudentName);
+        var studentLookup = students.ToLookup(s => s.ClassID, s => s.StudentName);
+
+        foreach (var group in studentLookup)
+        {
+            Console.WriteLine("Age Group: {0}", string.Join(',', group.Key));  //每组都有一个键 
+
+            foreach (var s in group)  //每个组都有一个内部集合  
+                Console.WriteLine("Student Name: {0}", s);
+        }
+
+        var firstStudent = students.First();
+        var firstAdult = students.FirstOrDefault(s => s.Birthday <= DateTime.Now.AddYears(-18));
+        var onlyWangWu = students.Single(s => s.StudentName == "王五");
+        var wangWuOrDefault = students.SingleOrDefault(s => s.StudentName == "王六");
+        var lastStudent = students.Last();
+        var lastAdult = students.LastOrDefault(s => s.Birthday <= DateTime.Now.AddYears(-18));
+        var secondStudent = students.ElementAt(1);
+        var tenthStudentOrDefault = students.ElementAtOrDefault(9);
+        var nonEmptyStudents = students.DefaultIfEmpty(new StudentInfo { StudentID = 0, StudentName = "默认Student", Address = "默认" });
+    }
+
+    #region 学生信息测试
+
+    public class StudentInfo
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public DateTime Birthday { get; set; }
+        public int ClassID { get; set; }
+        public string Address { get; set; }
+        public List<Course> Courses { get; set; } = new List<Course>();
+    }
+
+    public class Course
+    {
+        public int CourseID { get; set; }
+        public string CourseName { get; set; }
+    }
+
+    private static List<StudentInfo> students = new List<StudentInfo>
+    {
+        new StudentInfo
+        {
+            StudentID=1,
+            StudentName="大姚",
+            Birthday=Convert.ToDateTime("1997-10-25"),
+            ClassID=101,
+            Courses = new List<Course>
+            {
+                new Course { CourseID = 101, CourseName = "语文" },
+                new Course { CourseID = 102, CourseName = "数学" }
+            }
+        },
+        new StudentInfo
+        {
+            StudentID=2,
+            StudentName="李四",
+            Birthday=Convert.ToDateTime("1998-10-25"),
+            ClassID=101,
+            Courses = new List<Course>
+            {
+                new Course { CourseID = 101, CourseName = "语文" },
+                new Course { CourseID = 102, CourseName = "数学" }
+            }
+        },
+        new StudentInfo
+        {
+            StudentID=3,
+            StudentName="王五",
+            Birthday=Convert.ToDateTime("1999-10-25"),
+            ClassID=102,
+            Address="广州",
+            Courses = new List<Course>
+            {
+                new Course { CourseID = 101, CourseName = "语文" },
+                new Course { CourseID = 102, CourseName = "数学" }
+            }
+        },
+        new StudentInfo
+        {
+            StudentID=4,
+            StudentName="时光者",
+            Birthday=Convert.ToDateTime("1999-11-25"),
+            ClassID=102,
+            Address="深圳" ,
+            Courses = new List<Course>
+            {
+                new Course { CourseID = 104, CourseName = "历史" },
+                new Course { CourseID = 103, CourseName = "地理" }
+            }
+        }
+    };
+
+    #endregion
+}
